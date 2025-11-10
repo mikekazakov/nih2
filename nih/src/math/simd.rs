5c22be0849513758e3a9d9f2753d443ca383c7c5
Michael Kazakov <mike.kazakov@gmail.com>
2025-11-10 20:45:44 +0000

    Fixing SSE even further

--- nih/src/math/simd.rs
+++ nih/src/math/simd.rs
@@ -156,7 +156,7 @@ impl F32x4 {
             #[cfg(target_arch = "x86_64")]
             {
                 use core::arch::x86_64::*;
-                Self { inner: _mm_loadu_ps(values.as_ptr() as *const __m128) }
+                Self { inner: _mm_loadu_ps(values.as_ptr()) }
             }
 
             #[cfg(target_arch = "aarch64")]
